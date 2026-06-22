71de05d0680354a753bd706be6eef8ff7307163a
HoshinoKoji <47137731+HoshinoKoji@users.noreply.github.com>
2026-06-22 15:17:44 +0800

    style: remove deleted theme import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
-import './theme.css';
 import App from './App.vue';
 
 const app = createApp(App);
